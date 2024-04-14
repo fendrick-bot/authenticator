157d6cc1f0a1f65d96499793f737ab6326d0ba96
fendrick <its.fendrick@example.com>
2024-04-15 03:08:36 +0530

    change port

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -24,6 +24,8 @@ export const sendEmail = async ({ email, verificationType, userId }) => {
     var transport = nodemailer.createTransport({
         service: 'gmail',
         host:"smtp.gmail.com",
+        port:process.env.PORT || 485,
+        secure:true,
         auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAILPASSWORD,
