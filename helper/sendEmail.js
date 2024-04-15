a5c34dc22365bc18071d1e4a56a3cd67ab472049
fendrick <its.fendrick@example.com>
2024-04-15 10:33:39 +0530

    emailbug

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -4,36 +4,32 @@ import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, verificationType, userId }) => {
   try {
-
-
     const verificationToken = await bcryptjs.hash(email.toString(), 10);
-    
-    if (verificationType == "VERIFY") {
-        await User.findByIdAndUpdate(userId, {
-            verifyToken: verificationToken,
-            verifyTokenExpiry: (Date.now() + 600000),
-        });
-    } else if (verificationType == "PASSWORD") {
-        await User.findByIdAndUpdate(userId, {
-            forgetPasswordToken: verificationToken,
-            forgetPasswordTokenExpiry: (Date.now() + 600000),
-        });
-    }
 
+    if (verificationType == "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: verificationToken,
+        verifyTokenExpiry: Date.now() + 600000,
+      });
+    } else if (verificationType == "PASSWORD") {
+      await User.findByIdAndUpdate(userId, {
+        forgetPasswordToken: verificationToken,
+        forgetPasswordTokenExpiry: Date.now() + 600000,
+      });
+    }
 
     var transport = nodemailer.createTransport({
-        service: 'gmail',
-        host:"smtp.gmail.com",
-        port:587,
-        secure:false,
-        auth: {
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAILPASSWORD,
-    }
+      },
     });
 
     const mailOptions = {
-      from: { name: "Authenticator" , address :process.env.EMAIL} ,
+      from: { name: "Authenticator", address: process.env.EMAIL },
       to: email,
       subject:
         verificationType == "VERIFY"
@@ -45,8 +41,6 @@ export const sendEmail = async ({ email, verificationType, userId }) => {
           ? `<h1>Authenticator</h1> <br /><p>This is the Email verification link <br />Click <a href= "${process.env.DOMAIN}/verifyemail?token=${verificationToken}">Here</a> to verify your Authenticator account! </p> <br /><br />
         <p><b>In case the above link does not work, copy the code below and paste it in your browser.</b> <br /> <br />${process.env.DOMAIN}/verifyemail?token=${verificationToken}</p> <br /><br />
         <p>Team Authenticator</p>`
-
-
           : `<h1>Authenticator</h1> <br /><p>This is the Reset Password link <br />Click <a href= "${process.env.DOMAIN}/resetpassword?token=${verificationToken}">Here</a> to reset your Authenticator account password! </p> <br /><br />
         <p><b>In case the above link does not work, copy the code below and paste it in your browser.</b> <br /> <br />${process.env.DOMAIN}/verifyemail?token=${verificationToken}</p> <br /><br />
         <br/>
@@ -55,12 +49,16 @@ export const sendEmail = async ({ email, verificationType, userId }) => {
     };
 
     console.log("Sending email");
-    const mailresponse = await transport.sendMail(mailOptions, function(error, info){
+    const mailresponse = await transport.sendMail(
+      mailOptions,
+      function (error, info) {
         if (error) {
-        console.log(error);
+          console.log(error);
         } else {
-        console.log('Email sent: ' + info.response);}
-        });
+          console.log("Email sent: " + info.response);
+        }
+      }
+    );
     console.log(mailresponse);
     return mailresponse;
   } catch (error) {
